c995f081202f81c7de8920760d8be4f8439b757f
Iori OSADA <iori.osada@gmail.com>
2021-08-27 19:06:41 +0900

    fix: add limit for performance

--- app/controllers/youtube_videos_controller.rb
+++ app/controllers/youtube_videos_controller.rb
@@ -4,7 +4,7 @@ class YoutubeVideosController < ApplicationController
   def index
     @channel = YoutubeChannel.find_by(channel_id: params[:channel_id])
     @channel ||= YoutubeChannel.take # UClO5iosel11ZtWTwYoYMa9w
-    @videos = @channel.videos
+    @videos = @channel.videos.limit(200)
   end
 
   def show
