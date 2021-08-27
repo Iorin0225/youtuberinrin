03e72b3487dba54828b37568a682add63bba2bf3
Iori OSADA <iori.osada@gmail.com>
2021-08-27 21:29:01 +0900

    feat: add query param

--- app/controllers/youtube_videos_controller.rb
+++ app/controllers/youtube_videos_controller.rb
@@ -5,6 +5,7 @@ class YoutubeVideosController < ApplicationController
     @channel = YoutubeChannel.find_by(channel_id: params[:channel_id])
     @channel ||= YoutubeChannel.take
     @videos = @channel.videos
+    @videos = process_search_query(search_query) if search_query.present?
     @videos = @videos.eager_load(:markers)
     @videos = @videos.order('youtube_videos.published_at DESC, youtube_video_markers.seconds ASC')
   end
@@ -15,5 +16,16 @@ class YoutubeVideosController < ApplicationController
     @channel = @videos.take.channel
 
     render :index
+  end
+
+  private
+
+  def process_search_query(query)
+    like_query = "%#{query}%"
+    @videos.where('youtube_videos.title LIKE ? OR youtube_videos.description LIKE ?', like_query, like_query)
+  end
+
+  def search_query
+    params[:query]
   end
 end
