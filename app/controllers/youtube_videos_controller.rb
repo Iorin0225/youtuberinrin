5a1307489a35d6f0426dc3f171f4521300b08316
Iori OSADA <iori.osada@gmail.com>
2021-08-27 21:11:06 +0900

    fix: N+1 again

--- app/controllers/youtube_videos_controller.rb
+++ app/controllers/youtube_videos_controller.rb
@@ -4,7 +4,9 @@ class YoutubeVideosController < ApplicationController
   def index
     @channel = YoutubeChannel.find_by(channel_id: params[:channel_id])
     @channel ||= YoutubeChannel.take
-    @videos = @channel.videos.eager_load(:markers)
+    @videos = @channel.videos
+    @videos = @videos.eager_load(:markers)
+    @videos = @videos.order('youtube_videos.published_at DESC, youtube_video_markers.seconds ASC')
   end
 
   def show
